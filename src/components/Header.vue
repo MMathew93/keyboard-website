90dc1ef4d4c1c4c35de265cf9b8329b6fcd03b9d
lyndexer <mckinney.m93@gmail.com>
2020-09-28 10:20:53 -0700

    Adding basics to repo

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <p>
+      This is pretty much the reusable code to set up in header for all view
+      routes
+    </p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
