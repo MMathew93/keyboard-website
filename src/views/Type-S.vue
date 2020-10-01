eeb1180da88a6f5131d1912dbfb47b75d323cdd4
lyndexer <mckinney.m93@gmail.com>
2020-10-01 11:35:40 -0700

    set up template for main products page

--- src/views/Type-S.vue
+++ src/views/Type-S.vue
@@ -1,6 +1,114 @@
 <template>
   <div class="type-s">
     <Header />
+    <section>
+      <div class="columns">
+        <div class="column">
+          <div class="content container">
+            <div class="product-image">
+              <img
+                src="@/assets/products/keyboards/type-s/297301rev.png"
+                width="250"
+                height="250"
+              />
+            </div>
+            <div class="info">
+              <ul>
+                <li>Product ID here</li>
+                <li>Color: Charcoal</li>
+                <li>Key Caps: Blank/Non-Printed</li>
+              </ul>
+            </div>
+            <div class="buttons container">
+              <div class="button-box">
+                <b-button type="is-primary"> Info </b-button>
+              </div>
+              <div class="button-box">
+                <b-button type="is-danger"> Add to cart </b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content container">
+            <div class="product-image">
+              <img
+                src="@/assets/products/keyboards/type-s/297101rev.png"
+                width="250"
+                height="250"
+              />
+            </div>
+            <div class="info">
+              <ul>
+                <li>Product ID here</li>
+                <li>Color: White</li>
+                <li>Key Caps: Blank/Non-Printed</li>
+              </ul>
+            </div>
+            <div class="buttons container">
+              <div class="button-box">
+                <b-button type="is-primary"> Info </b-button>
+              </div>
+              <div class="button-box">
+                <b-button type="is-danger"> Add to cart </b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content container">
+            <div class="product-image">
+              <img
+                src="@/assets/products/keyboards/type-s/297201rev.png"
+                width="250"
+                height="250"
+              />
+            </div>
+            <div class="info">
+              <ul>
+                <li>Product ID here</li>
+                <li>Color: Charcoal</li>
+                <li>Key Caps: Stamped</li>
+              </ul>
+            </div>
+            <div class="buttons container">
+              <div class="button-box">
+                <b-button type="is-primary"> Info </b-button>
+              </div>
+              <div class="button-box">
+                <b-button type="is-danger"> Add to cart </b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="content container">
+            <div class="product-image">
+              <img
+                src="@/assets/products/keyboards/type-s/297001rev.png"
+                width="250"
+                height="250"
+              />
+            </div>
+            <div class="info">
+              <ul>
+                <li>Product ID here</li>
+                <li>Color: White</li>
+                <li>Key Caps: Stamped</li>
+              </ul>
+            </div>
+            <div class="buttons container">
+              <div class="button-box">
+                <b-button type="is-primary"> Info </b-button>
+              </div>
+              <div class="button-box">
+                <b-button type="is-danger"> Add to cart </b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <Footer />
   </div>
 </template>
@@ -18,4 +126,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+section {
+  margin: 2%;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.info {
+  color: white;
+  width: 241px;
+}
+
+.button-box {
+  margin: 15px;
+}
+</style>
